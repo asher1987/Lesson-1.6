bad453b5ffd2e3d507fde4b9e9c607409096eb41
Ashley Likes <alikes1987@gmail.com>
2022-10-24 11:20:09 -0400

    Initial Commit

---
+++ Lesson 1.6/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lesson 1.6
+//
+//  Created by admin on 10/24/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
